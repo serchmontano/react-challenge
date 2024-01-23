d6085d4be97bbc70b8b968fee8fbc9d3db30bcee
SmontanoEAmiloz <102571729+SmontanoEAmiloz@users.noreply.github.com>
2024-01-22 19:45:16 -0600

    Add readme.md

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-challenge",
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [tailwindcss],
+      plugins: [tailwindcss()],
     },
   }
 })
